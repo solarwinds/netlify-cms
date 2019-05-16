21c587a56c07f08cbddf6d2e261710b29c4b24ff
Max Gardner <max.c.gardner@gmail.com>
2019-05-16 02:03:22 -0500

    Update Drive media library
    - Validate config options
    - Handle post-processing of file
    - Refine relative path method

--- packages/netlify-cms-core/src/mediaLibrary.js
+++ packages/netlify-cms-core/src/mediaLibrary.js
@@ -4,7 +4,6 @@
  */
 import { once } from 'lodash';
 import { getMediaLibrary } from 'Lib/registry';
-import { getCurrentEntry, getCurrentRoute } from 'Lib/store';
 import store from 'ReduxStore';
 import { createMediaLibrary, insertMedia } from 'Actions/mediaLibrary';
 
@@ -19,14 +18,22 @@ store.subscribe(() => {
   const state = store.getState();
   const mediaLibraryName = state.config.getIn(['media_library', 'name']);
   if (mediaLibraryName && !state.mediaLibrary.get('externalLibrary')) {
-    const mediaLibraryConfig = {
-      ...state.config.get('media_library').toJS(),
-      routing: {
-        collectionSlugs: state.config.get('collections').toJS().map(coll => { return { name: coll.name, slug: coll.slug }}),
-        getCurrentRoute: getCurrentRoute(store),
-        getCurrentEntry: getCurrentEntry(store)
-      }
-    };
+    const mediaLibraryConfig = state.config.get('media_library').toJS();
+    if (mediaLibraryConfig.config.enable_nested_folders) {
+      mediaLibraryConfig.pathConstructor = {
+        getCollectionFolder: (function(store) {
+          return () => {
+            const state = store.getState();
+            const collectionName = state.entryDraft.get('entry').toJS().collection;
+            const mediaSubfolder = state.collections.get(collectionName).toJS().media_subfolder || null;
+            if (mediaSubfolder) {
+              return mediaSubfolder;
+            }
+            return collectionName;
+          }
+        })(store)
+      };
+    }
     initializeMediaLibrary(mediaLibraryName, mediaLibraryConfig);
   }
 });
